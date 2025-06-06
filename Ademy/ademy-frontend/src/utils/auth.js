c2b142bbca08b05be275158aabda46f9c56e4d75
Vigneshxvicky <vignesh26vic@gmail.com>
2025-06-06 07:53:54 +0530

    api url has been changed

--- Ademy/ademy-frontend/src/utils/auth.js
+++ Ademy/ademy-frontend/src/utils/auth.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export async function login(authForm) {
-  const response = await axios.post("http://localhost:5000/api/login", authForm);
+  const response = await axios.post("https://student-guide-backend.onrender.com/api/login", authForm);
   return response.data;
 }
 
 export async function register(authForm) {
-  const response = await axios.post("http://localhost:5000/api/register", authForm);
+  const response = await axios.post("https://student-guide-backend.onrender.com/api/register", authForm);
   return response.data;
 }
